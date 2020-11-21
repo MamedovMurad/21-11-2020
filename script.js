a1da4761fc6485538f3cc1de4f4b6fad49e88b9e
Mamedov Murad <muradvm@code.edu.az>
2020-11-21 09:36:42 +0400

    array

---
+++ script.js
@@ -0,0 +1,10 @@
+const arr = [1,2,3,4,5,66,77,88,123];
+
+for (let index = 0; index < arr.length; index++) {
+  
+    if (arr[index]>50&&arr[index]<100) {
+        
+        console.log(arr[index]);
+    }
+    
+}
